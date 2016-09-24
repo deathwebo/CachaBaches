3603beaad85aa89b28562fcf88c135814e1eb62c
Erick Márquez <tony3c24@gmail.com>
2016-09-24 07:02:46 -0700

    Agregada detección de Shake y Geolocalización

--- index.android.js
+++ index.android.js
@@ -6,59 +6,128 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-    DeviceEventEmitter
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    DeviceEventEmitter,
+    TouchableHighlight,
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 var mSensorManager = require('NativeModules').SensorManager;
 
-mSensorManager.startAccelerometer(100);
-
-console.log('hello world!');
-DeviceEventEmitter.addListener('Accelerometer', function(data) {
-    // console.log(data);
-});
-// mSensorManager.stopAccelerometer();
 
 class CacheBaches extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-            Bienvendo al Cache Baches!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    state = {
+        currentPosition: null,
+        accelX: null,
+        accelY: null,
+        accelZ: null,
+        shakeTreshold: 10,
+        lastUpdate: null
+    }
+
+    onPress() {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                var currentPosition = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    latitudeDelta: 0,
+                    longitudeDelta: 0,
+                }
+                this.setState({currentPosition})
+            },
+            (error) => alert(JSON.stringify(error)),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+        );
+    }
+
+    componentDidMount() {
+        console.log('asked for the initial position')
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                var currentPosition = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    latitudeDelta: 0,
+                    longitudeDelta: 0,
+                }
+                this.setState({currentPosition})
+                console.log('position retrieved');
+            },
+            (error) => alert(JSON.stringify(error)),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+        );
+
+
+        mSensorManager.startAccelerometer(100);
+
+        DeviceEventEmitter.addListener('Accelerometer', (data) => {
+            if(this.state.accelX == null && this.state.accelY == null
+            && this.state.accelZ == null) {
+                this.setState({ accelX: data.x});
+                this.setState({accelY: data.y});
+                this.setState({accelZ: data.z});
+            }
+            else{
+                deltaX = Math.abs(this.state.accelX - data.x);
+                deltaY = Math.abs(this.state.accelY - data.y);
+                deltaZ = Math.abs(this.state.accelZ - data.z);
+
+                if( (deltaX > this.state.shakeTreshold  && deltaY > this.state.shakeTreshold)
+                || (deltaX > this.state.shakeTreshold && deltaZ > this.state.shakeTreshold)
+                || (deltaY > this.state.shakeTreshold && deltaZ > this.state.shakeTreshold) ) {
+                    var now = Date.now();
+                    if(this.state.lastUpdate == null
+                        || ( (now - this.state.lastUpdate) > 100 )) {
+
+                        alert('Shake it baby!');
+                    }
+                }
+            }
+        });
+// mSensorManager.stopAccelerometer();
+
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableHighlight
+                    style={styles.welcome}
+                    onPress={() => this.onPress() }>
+                    <Text>Hello!</Text>
+                </TouchableHighlight>
+
+                <MapView
+                    style={styles.map}
+                    showsUserLocation={true}
+                    followsUserLocation={true}
+                    region={this.state.currentPosition}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+    },
+    welcome: {
+        margin: 10,
+        backgroundColor: 'red',
+        elevation: 3,
+        height: 30,
+        width: 50
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
 });
+
 
 AppRegistry.registerComponent('CacheBaches', () => CacheBaches);
